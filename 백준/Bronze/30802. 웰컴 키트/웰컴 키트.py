48bd3d2202e1a92763badd2ec33ed15b7ce517c7
kty5788 <63940620+kty5788@users.noreply.github.com>
2024-06-11 17:28:25 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,12 +1,5 @@
 n = int(input())
 size = list(map(int,input().split()))
 t,p = list(map(int,input().split()))
-cnt = 0
-
-for i in size:
-    cnt += i//t
-    i %= t
-    if i != 0:
-        cnt += 1
-print(cnt)
-print(n//p,n%p)
+print(sum(map(lambda x:(x-1) // t+1,size)))
+print(n//p ,n%p)
