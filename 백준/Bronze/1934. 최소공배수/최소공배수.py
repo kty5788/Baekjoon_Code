00c293678ae7e0b10ca8ece7c5468256aaca9f9c
kty5788 <63940620+kty5788@users.noreply.github.com>
2024-07-13 13:05:02 +0900

    [Bronze I] Title: 최소공배수, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,12 @@
+t = int(input())
+for i in range(t):
+    a,b = map(int,input().split())
+    if b > a:
+        a,b = b,a
+    f = a*b
+    while b != 0:
+        if a%b == 0:
+            break
+        else:
+            a,b = b,a%b
+    print(int(f/b))
