50807838991bbe436aeeebb967d54deffe98727f
kty5788 <63940620+kty5788@users.noreply.github.com>
2024-07-28 22:46:07 +0900

    [Silver IV] Title: 현대모비스 선풍기 굴리기, Time: 44 ms, Memory: 33240 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31534. 현대모비스 선풍기 굴리기/현대모비스 선풍기 굴리기.py
+++ 백준/Silver/31534. 현대모비스 선풍기 굴리기/현대모비스 선풍기 굴리기.py
@@ -3,6 +3,12 @@ import math
 input = sys.stdin.readline
 
 a,b,h = map(int,input().split())
-lost_h = (a*h)/(b-a)
 
-print(round(((b**2+(lost_h+h)**2)-(a**2+lost_h**2))*math.pi,7))
+if a > b:
+    a,b=b,a
+
+if a != b:
+    lost_h = (a*h)/(b-a)
+    print(round(((b**2+(lost_h+h)**2)-(a**2+lost_h**2))*math.pi,7))
+else:
+    print(-1)
