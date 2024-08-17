44d6c1c1abc4c7f34a3075b2f5be38d2241f38ee
kty5788 <63940620+kty5788@users.noreply.github.com>
2024-08-17 17:08:54 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -3,12 +3,13 @@ cnt = -1
 
 def divide_n_conquer(row,col,n):
     global cnt
-    if n == 0:
+    if n == 0: # 마지막 1칸이 되었을떄
         cnt += 1
         if row == r and col == c:
             print(cnt)
             exit()
-    else:
+    else: # 2 ** n 칸일 떄 분할정복
+        # r행 c열이 해당되는 부분만 분할정복
         divide_cntnum = 2**(n-1) * 2**(n-1)
         if r <= row+2**n//2-1 and c <= col+2**n//2-1:
             divide_n_conquer(row, col, n-1)
