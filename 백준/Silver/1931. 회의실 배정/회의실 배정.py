c7e1bce2be81b3587a7d69971223a6f0a637fba4
kty5788 <63940620+kty5788@users.noreply.github.com>
2024-08-24 00:22:23 +0900

    [Silver I] Title: 회의실 배정, Time: 2656 ms, Memory: 60092 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+n = int(input())
+l = []
+for i in range(n):
+    l.append(list(map(int,(input().split()))))
+
+l.sort(key= lambda x : [x[1],x[0]])
+
+cnt = 0
+end_time = -1
+
+for i in range(len(l)):
+    if end_time == -1:
+        end_time = l[i][1]
+        cnt += 1
+    else:
+        if end_time <= l[i][0]:
+            end_time = l[i][1]
+            cnt += 1
+
+print(cnt)
