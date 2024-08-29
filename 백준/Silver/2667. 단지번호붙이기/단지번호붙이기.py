d45d052157a15a41b87ef508fc7c9c3db793f06a
kty5788 <63940620+kty5788@users.noreply.github.com>
2024-08-29 15:04:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -10,9 +10,10 @@ house_list = []
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def dfs(i,j,cnt=0):
+def dfs(i,j,cnt=1):
     stack = []
     stack.append([i,j])
+    visited[i][j] = True
     
     while stack:
         x,y = stack.pop()
@@ -26,16 +27,14 @@ def dfs(i,j,cnt=0):
                     stack.append([nx,ny])
                     visited[nx][ny] = True
                     cnt += 1
+
     return cnt
 
 for i in range(n):
     for j in range(n):
         if house[i][j] == '1' and not visited[i][j]:
             num = dfs(i,j)
-            if num == 0:
-                house_list.append(1)
-            else:
-                house_list.append(num)
+            house_list.append(num)
             res += 1
 
 house_list.sort()
