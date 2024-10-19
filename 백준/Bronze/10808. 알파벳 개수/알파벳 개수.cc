50172992ab528a8253d035c7da7ffe35603910aa
kty5788 <63940620+kty5788@users.noreply.github.com>
2024-10-20 01:05:08 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main() {
-	string s;
+	char s[101];
 	int alphabet[27] = {0,};
 	cin >> s;
 
-	for (int i = 0; i < s.size(); i++) {
-		alphabet[int(s[i])-'a'] += 1;
+	for (int i = 0; s[i]; i++) {
+		alphabet[s[i]-'a']++;
 	}
 
 	for (int i = 0; i < 26; i++) {
